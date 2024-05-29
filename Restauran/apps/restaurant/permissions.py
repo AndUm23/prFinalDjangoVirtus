854643ff5d38f0a76cf5a631f0013abf150e597a
Andrés <74312233+AndUm23@users.noreply.github.com>
2024-05-28 19:44:54 -0500

    Update permissions.py

--- Restauran/apps/restaurant/permissions.py
+++ Restauran/apps/restaurant/permissions.py
@@ -3,9 +3,9 @@ from rest_framework import permissions
 class IsManagerOrAdminTables(permissions.BasePermission):
     def has_permission(self, request, view):
         # Check if the user has the necessary role (Manager or AdminTables)
-        return request.user.waiter.charge in ['MANAGER', 'ADMINTABLES']
+        return request.user.waiter.charge in ['MG', 'AT']
 
 class IsManager(permissions.BasePermission):
     def has_permission(self, request, view):
         # Check if the user has the necessary role (Manager)
-        return request.user.waiter.charge == 'MANAGER'
+        return request.user.waiter.charge == 'MG'
